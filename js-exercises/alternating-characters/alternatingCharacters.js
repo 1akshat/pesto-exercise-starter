46c38cd0bc27d340d473245d723acd744cbc889a
Akki <akshatuppal_vijay@srmuniv.edu.in>
2020-03-14 14:33:42 +0530

    Week2 (#2)
    * week 1 solutions
    
    * add skills video link
    
    * week2 solutions added
    
    Co-authored-by: akki <akshat@shopinsync.com>

---
+++ js-exercises/alternating-characters/alternatingCharacters.js
@@ -0,0 +1,21 @@
+function alternatingCharacters(arrOfStrings) {
+  var alternatingCharactersCount = []
+	arrOfStrings.forEach(function(singleString) {
+		let counter = 0;
+     if (singleString.length >= 1 && singleString.length <= 100000) {
+     	let stringArray = singleString.split('');
+     	let past = stringArray[0];
+     	stringArray = stringArray.filter((item, key) => {
+     		if (item !== past && key !== 0) {
+     			past = item;
+     			return item; 
+     		}  
+     	});
+     	counter = singleString.length - [ singleString.charAt(0), ...stringArray ].length;
+     }
+     alternatingCharactersCount.push(counter)
+	})
+  return alternatingCharactersCount
+}
+
+export { alternatingCharacters };
